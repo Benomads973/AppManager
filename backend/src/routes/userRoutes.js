9a2d3de5dda5cbbd711b215c805dce4331cab5ba
Administrator <admin@example.com>
2024-09-20 20:34:01 +0200

    Update

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -58,21 +58,3 @@ router.post('/login', async (req, res) => {
 });
 
 module.exports = router;
-apt-get update -y
-apt-get install -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
- 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-apt-key fingerprint 0EBFCD88
- 
-add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
-apt-get update -y
-apt-get install -y docker-ce
- 
-usermod -aG docker $USER
